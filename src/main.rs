556b4d1b2ac0ee7ebf562686af95e14195b629d1
Luan <luancgs.dev@pm.me>
2024-10-05 14:38:35 -0300

    chore: creating basic ratatui app with interaction

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,107 @@
 use ::std::io;
 
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Alignment, Rect},
     style::Stylize,
-    widgets::Paragraph,
-    DefaultTerminal,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{
+        block::{Position, Title},
+        Block, Paragraph, Widget,
+    },
+    DefaultTerminal, Frame,
 };
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
     terminal.clear()?;
-    let app_result = run(terminal);
+    let app_result = App::default().run(&mut terminal);
     ratatui::restore();
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let bg_color: ratatui::style::Color = "#0000ff".parse().unwrap();
-            let fg_color: ratatui::style::Color = "#ffffff".parse().unwrap();
-            let greeting = Paragraph::new("Hello, Ratatui! (press 'q' to quit)")
-                .bg(bg_color)
-                .fg(fg_color);
-            frame.render_widget(greeting, frame.area());
-        })?;
+#[derive(Debug, Default)]
+pub struct App {
+    counter: u8,
+    exit: bool,
+}
 
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
+impl App {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key) if key.kind == KeyEventKind::Press => self.handle_key(key),
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: KeyEvent) {
+        if key.kind == KeyEventKind::Press {
+            match key.code {
+                KeyCode::Char('q') => self.exit = true,
+                KeyCode::Left => self.decrement_counter(),
+                KeyCode::Right => self.increment_counter(),
+                _ => {}
             }
         }
+    }
+
+    fn increment_counter(&mut self) {
+        self.counter += 1;
+    }
+
+    fn decrement_counter(&mut self) {
+        self.counter -= 1;
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Counter App Tutorial ".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]));
+
+        let block = Block::bordered()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(Position::Bottom),
+            )
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.counter.to_string().bold().into(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
