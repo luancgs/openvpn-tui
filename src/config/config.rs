d445c8e8396fdb3cd8c3e4770adf43c5cdf148f3
Luan <luancgs.dev@pm.me>
2024-10-05 22:46:17 -0300

    feat: adds possibility to configure vpn files path

---
+++ src/config/config.rs
@@ -0,0 +1,67 @@
+use serde::Deserialize;
+use std::{fs, process};
+
+use dirs;
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub vpn_files_path: String,
+}
+
+impl Config {
+    fn get_config_path() -> String {
+        let config_dir = dirs::config_dir();
+
+        match config_dir {
+            Some(path) => path.to_str().unwrap().to_string(),
+            None => {
+                eprintln!("Could not find the config directory");
+                process::exit(1);
+            }
+        }
+    }
+
+    pub fn default() -> Self {
+        let home_dir = dirs::home_dir();
+
+        let config = match home_dir {
+            Some(path) => match path.to_str() {
+                Some(path) => {
+                    let vpn_files_path = format!("{}/.vpns", path);
+                    Config { vpn_files_path }
+                }
+                None => {
+                    eprintln!("Could not convert the home directory to a string");
+                    process::exit(1);
+                }
+            },
+            None => {
+                eprintln!("Could not find the home directory");
+                process::exit(1);
+            }
+        };
+
+        config
+    }
+
+    pub fn from_file() -> Self {
+        let config_path = Config::get_config_path();
+        let config_file = format!("{}/openvpn-tui.toml", config_path);
+
+        let config = match fs::read_to_string(config_file) {
+            Ok(contents) => match toml::from_str(&contents) {
+                Ok(config) => config,
+                Err(_) => {
+                    eprintln!("Could not parse the config file");
+                    process::exit(1);
+                }
+            },
+            Err(_) => {
+                eprintln!("Could not read the config file");
+                process::exit(1);
+            }
+        };
+
+        config
+    }
+}
